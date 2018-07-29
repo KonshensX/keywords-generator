fc8d77bd31aec51f2c1062491a4c4896e0416fb4
konshensx16 <konshensx16@gmail.com>
2018-07-29 20:09:48 +0100

    Initial commit, keywords generator which generates file based on the content of a file "model" and using the yaml file

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+require_once('vendor/autoload.php');
+
+use Symfony\Component\Yaml\Parser;
+
+// I NEED TO OPEN THE RIGHT MODEL FOR EACH ITERRATION
+
+// parse the file
+$yaml = new Parser();
+$content = $yaml->parse(file_get_contents('content.yaml'));
+// array of regions
+$regions = explode(";", $content['regions']);
+
+// iterate over all regions
+for ($regionIndex = 0; $regionIndex < count($regions); $regionIndex++ )
+{
+    // iterate over the models
+    foreach ($content as $key => $value)
+    {
+        // if it's the regions "which is the last thing in content array", just ignore it
+        if ($key === 'regions')
+            break;
+
+        // get the correct model number; ex:1
+        $modelNumber = mb_substr($key, -1);
+
+        // Open the correct model file
+        $modelName = 'model' . $modelNumber . '.php';
+        $handle = fopen($modelName, 'r');
+        $data = fread($handle,filesize($modelName));
+
+        // get the keywords out into an array or something :)
+        $keywords = explode(";", $value['keywords']);
+        // iterate over all the values and dump for now
+        for ($index = 0; $index < count($keywords); $index++) {
+            $keyword_string = $keywords[$index];
+            $region_string = $regions[$regionIndex];
+            // create a new file and save it to the disk
+            $newFilename = strtolower($keyword_string) . " " . strtolower($region_string);
+            $newFilename = str_replace(' ', '-', $newFilename) .'.php';
+
+            $newHandle = fopen($newFilename, 'w');
+            // replace the data on the model file.
+            $newContent = str_replace(['$motcle', '$ville'], [ucfirst($keyword_string), ucfirst($region_string)], $data);
+            fwrite($newHandle, $newContent);
+            fclose($newHandle);
+        }
+        // closing the file
+        fclose($handle);
+    }
+}
---
+++ model2.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="fr-FR" prefix="og: http://ogp.me/ns#">
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+    <meta name="description" content="Depuis 2009 One Shot Film travaille à valoriser vos messages et votre image à travers un film esthétique, fonctionnel et efficace. Notre atout : une compétence éprouvée dans la réalisation de supports vidéo aussi bien pour des PME que pour des grands groupes.">
+    <meta name="author" content="thomas landais">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge;chrome=1">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <title>$motcle $ville - HADA MODEL 2</title>
+    <meta name="description" content="$motcle $ville">
+    <meta name="title" content="ONE SHOT FILM">
+    <meta name="keywords" content="some keywords and stuff, this string is supposed to be very long">
+
+    <!-- For use in JS files -->
+    <script type="text/javascript">
+        var template_dir = "wp-content/themes/agile/index.html";
+    </script>
+
+    <link rel="profile" href="http://gmpg.org/xfn/11"/>
+
+    <link rel="icon" href="favicon.ico">
+
+    <link rel="pingback" href="xmlrpc.php"/>
+
+    <script type="text/javascript">var mo_options = mo_options || {};mo_options.disable_back_to_top = false;mo_options.sticky_menu = true;mo_options.theme_skin = "#df163c";mo_options.theme_directory_url = "wp-content/themes/agile/index.html";mo_options.slider_chosen="Revolution";mo_options.disable_smooth_page_load=false;mo_options.disable_animations_on_page=false;mo_options.disable_smooth_scroll=false;</script>
+
+    <!-- This site is optimized with the Yoast SEO plugin v4.3 - https://yoast.com/wordpress/plugins/seo/ -->
+    <link rel="canonical" href="index.html" />
+    <meta property="og:locale" content="fr_FR" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Accueil - One Shot Film - Société de production audiovisuelle" />
+    <meta property="og:url" content="http://www.oneshotfilm.fr/" />
+    <meta property="og:site_name" content="One Shot Film - Société de production audiovisuelle" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Accueil - One Shot Film - Société de production audiovisuelle" />
+    <meta name="twitter:site" content="@OneShotFilm" />
+    <meta name="twitter:creator" content="@OneShotFilm" />
+    <script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"WebSite","@id":"#website","url":"http:\/\/www.oneshotfilm.fr\/","name":"One Shot Film - Soci\u00e9t\u00e9 de production audiovisuelle","potentialAction":{"@type":"SearchAction","target":"http:\/\/www.oneshotfilm.fr\/?s={search_term_string}","query-input":"required name=search_term_string"}}</script>
+    <!-- / Yoast SEO plugin. -->
+
+    <link rel="alternate" type="application/rss+xml" title="One Shot Film - Société de production audiovisuelle &raquo; Flux" href="feed/index.html" />
+    <link rel="alternate" type="application/rss+xml" title="One Shot Film - Société de production audiovisuelle &raquo; Flux des commentaires" href="comments/feed/index.html" />
+    <link rel="alternate" type="application/rss+xml" title="One Shot Film - Société de production audiovisuelle &raquo; Accueil Flux des commentaires" href="home/feed/index.html" />
+    <link rel='stylesheet' id='contact-form-7-css'  href='wp-content/plugins/contact-form-7/includes/css/styles7e2e.css?ver=3.8.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='rs-settings-css'  href='wp-content/plugins/revslider/rs-plugin/css/settings4093.css?rev=4.1.4&amp;ver=3.8.26' type='text/css' media='all' />
+    <link rel='stylesheet' id='rs-captions-css'  href='wp-content/plugins/revslider/rs-plugin/css/dynamic-captions4093.css?rev=4.1.4&amp;ver=3.8.26' type='text/css' media='all' />
+    <link rel='stylesheet' id='rs-plugin-static-css'  href='wp-content/plugins/revslider/rs-plugin/css/static-captions4093.css?rev=4.1.4&amp;ver=3.8.26' type='text/css' media='all' />
+    <link rel='stylesheet' id='pretty-photo-css'  href='wp-content/themes/agile/css/prettyPhotoec3f.css?ver=3.8.26' type='text/css' media='screen' />
+    <link rel='stylesheet' id='icon-fonts-css'  href='wp-content/themes/agile/css/icon-fontsec3f.css?ver=3.8.26' type='text/css' media='screen' />
+    <link rel='stylesheet' id='style-theme-css'  href='wp-content/themes/agile/styleec3f.css?ver=3.8.26' type='text/css' media='all' />
+    <!--[if IE 8]>
+    <link rel='stylesheet' id='style-ie8-css'  href='http://www.oneshotfilm.fr/wp-content/themes/agile/css/ie8.css?ver=3.8.26' type='text/css' media='screen' />
+    <![endif]-->
+    <!--[if IE 9]>
+    <link rel='stylesheet' id='style-ie9-css'  href='http://www.oneshotfilm.fr/wp-content/themes/agile/css/ie9.css?ver=3.8.26' type='text/css' media='screen' />
+    <![endif]-->
+    <link rel='stylesheet' id='animate-css'  href='wp-content/themes/agile/css/animateec3f.css?ver=3.8.26' type='text/css' media='screen' />
+    <link rel='stylesheet' id='style-skin-php-css'  href='wp-content/themes/agile/css/skins/skinf970.css?skin=%23df163c&amp;ver=3.8.26' type='text/css' media='all' />
+    <link rel='stylesheet' id='style-plugins-css'  href='wp-content/themes/agile/css/pluginsec3f.css?ver=3.8.26' type='text/css' media='all' />
+    <script type='text/javascript' src='wp-includes/js/jquery/jquery3e5a.js?ver=1.10.2'></script>
+    <script type='text/javascript' src='wp-includes/js/jquery/jquery-migrate.min1576.js?ver=1.2.1'></script>
+    <script type='text/javascript' src='wp-content/plugins/revslider/rs-plugin/js/jquery.themepunch.plugins.min4093.js?rev=4.1.4&amp;ver=3.8.26'></script>
+    <script type='text/javascript' src='wp-content/plugins/revslider/rs-plugin/js/jquery.themepunch.revolution.min4093.js?rev=4.1.4&amp;ver=3.8.26'></script>
+    <script type='text/javascript' src='wp-content/themes/agile/js/libs/jquery.easing.1.3ec3f.js?ver=3.8.26'></script>
+    <script type='text/javascript' src='wp-includes/js/comment-reply.minec3f.js?ver=3.8.26'></script>
+    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc0db0.php?rsd" />
+    <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="wp-includes/wlwmanifest.xml" />
+    <meta name="generator" content="WordPress 3.8.26" />
+    <link rel='shortlink' href='indexd250.html?p=3982' />
+    <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-28879903-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <!-- Options based styling -->
+    <style type="text/css">
+        @import url(http://fonts.googleapis.com/css?family=Lato|Merriweather);
+
+        h1,h2,h3,h4,h5,h6 {font-family:"Lato";letter-spacing:1px;}
+        body{font-family:"Merriweather";}
+        body{}
+        #primary-menu > ul.menu > li > a {font-size:16px !important;color:#ffffff !important;}
+        #primary-menu ul.menu > li.sfHover > a, #primary-menu > ul.menu > li > a:hover { color:#df163c !important; }
+        #header {background-color:#000000;background-image: none;background-size: cover;}
+        #title-area .inner, #custom-title-area .inner,
+
+        #content, .sidebar-right-nav, .sidebar-left-nav
+        { opacity: 0; }
+        #pricing-action .pointing-arrow img { opacity: 0 }
+        #primary-menu > ul.menu > li  > a{font-size: 16px !important;padding: 0 30px;}
+    </style>
+
+</head>
+
+<body class="home page page-id-3982 page-template page-template-template-single-page-site-php layout-1c  fluid-width-page single-page-template">
+
+
+<a id="mobile-menu-toggle" href="#"><i class="icon-th-menu"></i>&nbsp;</a>
+<nav id="mobile-menu" class="menu-container clearfix"><div class="menu-header">Menu</div>
+    <ul id="menu-main" class="menu inner">
+        <?php helper::generateMenu() ?>
+    </ul></nav><!-- #mobile-menu -->
+<div id="container">
+
+
+
+    <header id="header" >
+
+        <div class="inner clearfix">
+
+            <div class="wrap">
+
+                <h1 id="site-logo"><a href="index.html" title="One Shot Film - Société de production audiovisuelle" rel="home"><img class="standard-logo" src="wp-content/uploads/2014/02/logo.png" alt="One Shot Film - Société de production audiovisuelle"/></a></h1><nav id="primary-menu" class="dropdown-menu-wrap clearfix">
+                    <ul id="menu-main-1" class="menu clearfix">
+                        <?php helper::generateMenu() ?>
+                    </ul></nav><!-- #primary-menu -->
+
+            </div>
+
+        </div>
+
+    </header>
+    <!-- #header -->
+
+
+    <style>
+        .pb {
+            padding-bottom: 200px;
+        }
+        .container {
+            width: 1170px;
+            margin: 0 auto 0 auto;
+        }
+    </style>
+
+    <!-- Start main-content -->
+    <div class="main-content">
+        <!-- Section: inner-header -->
+        <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="images/bg/bg11.jpg">
+            <div class="container pt-120 pb-50">
+                <!-- Section Content -->
+                <div class="section-content pt-100">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="title text-white">$motcle $ville</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <section class="container pb">
+            <div class="grid grid--container">
+                <div class="row text--center">
+                    <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 text-center">
+                        <div class="relative">
+                            <h2 class="title-uppercased">$motcle $ville</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+
+    <?php require_once('partials/footer.php') ?>
